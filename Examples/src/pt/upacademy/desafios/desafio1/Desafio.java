7f44f690cc11bfc40e9bdaff346eb64993f9d978
Joao Cortes <joaoscortes@gmail.com>
2019-12-11 19:15:56 +0000

    resolve desafio1

--- Examples/src/pt/upacademy/desafios/desafio1/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio1/Desafio.java
@@ -1,24 +1,26 @@
 package pt.upacademy.desafios.desafio1;
 
 public class Desafio {
+	private int num, big3Count = 0, big5Count = 0;
 
-	public Desafio(int i) {
-		// TODO Auto-generated constructor stub
+	public Desafio(int num) {
+		this.num = num;
 	}
 
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		for (int i = 1; i <= num; i++) {
+			if (i % 5 == 0)
+				big5Count++;
+			if (i % 3 == 0)
+				big3Count++;
+		}
 	}
 
 	public int getBig3Count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return big3Count;
 	}
 
 	public int getBig5Count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return big5Count;
 	}
-
 }
